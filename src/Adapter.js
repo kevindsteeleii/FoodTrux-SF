bfbd178102874f6317868d06ac4794268e1d9301
github username <github email address>
2019-02-12 16:27:59 -0500

    removed sensitive info

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import SECRET from './secret';
 const URLBase = 'http://bustime.mta.info/api/where/stops-for-location.json?'
 const Adapter = {
   defaultCall: async function(lat, long, diam=0.015) {
-    const RES = await axios.get(`${URLBase}lat=${lat}&lon=${long}latSpan=${diam}&lonSpan=${diam}&key=${}`)
+    const RES = await axios.get(`${URLBase}lat=${lat}&lon=${long}latSpan=${diam}&lonSpan=${diam}&key=${SECRET}`)
     return await RES.json();
-    // http://bustime.mta.info/api/where/stops-for-location.json?lat=40.8316&lon=-73.9099&latSpan=0.005&lonSpan=0.0015&key=4b675b38-8cd5-484d-bc9f-7ad20c1b60a8
+    // http://bustime.mta.info/api/where/stops-for-location.json?lat=40.8316&lon=-73.9099&latSpan=0.005&lonSpan=0.0015&key=<key>
   },
 }
 

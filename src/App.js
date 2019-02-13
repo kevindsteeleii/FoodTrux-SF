46178918972d724ed227af830c19b808c7b2fc5b
github username <github email address>
2019-02-13 14:39:11 -0500

    added leafletJS for map integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { MTA_BUS_KEY as SECRET } from './secret';
-// import adapter from './Adapter';
 import './stylesheets/App.scss';
 
 class App extends Component {
+
+  state = {
+    foodTrucks : [],
+    nearbyFoodTrucks: [],
+    longitude: null,
+    latitude: null,
+  }
+
+  async componentDidMount(){
+    const response = await axios('https://data.sfgov.org/resource/6a9r-agq8.json');
+    const data = await response.data;
+    this.setState({ foodTrucks: data });
+  }
+
   testButton = async(evt) => {
-    const URLBase = 'http://bustime.mta.info/api/where/stops-for-location.json?';
-    // adapter.defaultCall(40.8316, -73.9099)
-    const response = await axios(`${URLBase}lat=${40.8316}&lon=${-73.9099}&latSpan=${0.0015}&lonSpan=${0.0015}&key=${SECRET}`)
-    const data = await response.json()
-    console.log(data)
+    evt.persist();
   }
 
   render() {
-    return (
+    return (<>
       <div id="App">
         <button onClick={this.testButton}>Test</button><br/>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium unde, debitis molestiae tempore itaque aperiam. Optio laborum aperiam saepe quis magnam praesentium harum minus expedita repudiandae accusamus sequi eos, repellat in dicta illo temporibus at qui quo numquam amet!
       </div>
-    );
+      <div id="mapid"></div>
+    </>);
   }
 }
 

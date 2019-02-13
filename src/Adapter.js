3f30654c7bc8303c109fe52101ba76a7d27a31ac
github username <github email address>
2019-02-12 23:24:02 -0500

    fixing the problem with the data fetching

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,12 +1,34 @@
 import axios from 'axios';
-import SECRET from './secret';
-const URLBase = 'http://bustime.mta.info/api/where/stops-for-location.json?'
-const Adapter = {
-  defaultCall: async function(lat, long, diam=0.015) {
-    const RES = await axios.get(`${URLBase}lat=${lat}&lon=${long}latSpan=${diam}&lonSpan=${diam}&key=${SECRET}`)
-    return await RES.json();
-    // http://bustime.mta.info/api/where/stops-for-location.json?lat=40.8316&lon=-73.9099&latSpan=0.005&lonSpan=0.0015&key=<key>
+import {MTA_BUS_KEY as SECRET} from './secret';
+
+const URLBase = 'http://bustime.mta.info/api/where/stops-for-location.json?';
+ 
+// eslint-disable-next-line
+const proxy = {
+  host: '127.0.0.1',
+  port: 3000
+}
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin' : "*",
+    'X-Requested-With': 'XMLHttpRequest'
   },
+  proxy: proxy
+}
+
+
+const Adapter = {
+  defaultCall: async function(lat, long, diameter=0.0015) {
+    try {
+       const response = await axios.get(`${URLBase}lat=${lat}&lon=${long}&latSpan=${diameter}&lonSpan=${diameter}&key=${SECRET}`, config)
+       console.log(response)
+       return response;
+    } catch (err) {
+      console.log(err);
+    }
+     // http://bustime.mta.info/api/where/stops-for-location.json?lat=40.8316&lon=-73.9099&latSpan=0.005&lonSpan=0.0015&key=<key>
+  }
 }
 
 export default Adapter;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { MTA_BUS_KEY as SECRET } from './secret';
+// import adapter from './Adapter';
 import './stylesheets/App.scss';
 
 class App extends Component {
+  testButton = async(evt) => {
+    const URLBase = 'http://bustime.mta.info/api/where/stops-for-location.json?';
+    // adapter.defaultCall(40.8316, -73.9099)
+    const response = await axios(`${URLBase}lat=${40.8316}&lon=${-73.9099}&latSpan=${0.0015}&lonSpan=${0.0015}&key=${SECRET}`)
+    const data = await response.json()
+    console.log(data)
+  }
+
   render() {
     return (
       <div id="App">
+        <button onClick={this.testButton}>Test</button><br/>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium unde, debitis molestiae tempore itaque aperiam. Optio laborum aperiam saepe quis magnam praesentium harum minus expedita repudiandae accusamus sequi eos, repellat in dicta illo temporibus at qui quo numquam amet!
       </div>
     );
